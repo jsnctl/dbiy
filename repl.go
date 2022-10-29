05d20f4b0bf807bb44504a3d2d88f594baed750d
Jason Costello <jason.costello@synapt.ec>
2022-10-29 09:31:08 +0100

    Some REPL adjustments from Go examples

--- repl.go
+++ repl.go
@@ -4,21 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func prompt() {
 	fmt.Print("dbiy (0.0.1) > ")
 }
 
+func get(r *bufio.Reader) string {
+	t, _ := r.ReadString('\n')
+	return strings.TrimSpace(t)
+}
+
+func shouldContinue(text string) bool {
+	if strings.EqualFold("exit", text) {
+		return false
+	}
+	return true
+}
+
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-	for {
+	reader := bufio.NewReader(os.Stdin)
+	prompt()
+	text := get(reader)
+	for ; shouldContinue(text); text = get(reader) {
+		fmt.Println(text)
 		prompt()
-		for reader.Scan() {
-			text := reader.Text()
-			fmt.Print(text)
-			fmt.Println()
-			prompt()
-		}
 	}
 }
