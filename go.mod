4234e76315d806f48a12fcd01f4ac73787a25c86
Jason Costello <jason.costello@synapt.ec>
2022-10-29 01:07:26 +0100

    Init repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jsnctl/dbiy
+
+go 1.18
---
+++ repl.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func prompt() {
+	fmt.Print("diyb (0.0.1) > ")
+}
+
+func main() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		prompt()
+		for reader.Scan() {
+			text := reader.Text()
+			fmt.Print(text)
+			fmt.Println()
+			prompt()
+		}
+	}
+}
